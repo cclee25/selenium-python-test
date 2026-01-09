0e09ab4a6a00fbfd96268eea88ecee8c5904109e
cclee25 <christoffercanlaslee@gmail.com>
2026-01-09 15:53:38 +0800

    Updated test E2E purchase to accommodate Pytest framework

--- test_e2e_purchase.py
+++ test_e2e_purchase.py
@@ -6,38 +6,34 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-driver = webdriver.Chrome()
-driver.get("https://rahulshettyacademy.com/angularpractice/")
-driver.implicitly_wait(5)
+#Test data json file location
+file_loc = r"C:\Selenium_Python\data\shop_data.json"
 
-#Select a product
-driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
-products = driver.find_elements(By.CSS_SELECTOR, ".col-lg-3.col-md-6.mb-3")
-for product in products:
-    if product.find_element(By.CSS_SELECTOR, ".card.h-100 .card-body .card-title").text.strip() == "Blackberry":
-        product.find_element(By.CSS_SELECTOR, ".btn.btn-info").click()
-        break
+with open(file_loc) as file:
+    testing_data = json.load(file)
+    test_data = testing_data["data"]
 
-#Go to cart
-driver.find_element(By.XPATH, "//div[@id='navbarResponsive']/ul/li/a").click()
-product_name = driver.find_element(By.XPATH, "//div[@class='media-body']/h4[@class='media-heading']/a[normalize-space(text())='Blackberry']")
-assert product_name.text == "Blackberry"
+@pytest.mark.parametrize("test_data", test_data)
+def test_e2e(browser_instance, test_data):
+    driver = browser_instance
+    login_page = LoginPage(driver)
+    login_page.login(test_data["username"], test_data["password"])
+    time.sleep(3)
+    product_selection = ProductSelection(driver, test_data["product_name"])
+    product_selection.select_product(test_data["product_name"])
+    product_selection.go_to_cart(test_data["product_name"])
+    product_checkout = Checkout_Item(driver)
+    product_checkout.checkout(test_data["country_string"], test_data["selected_country"])
+    product_checkout.validate_purchase()
+    time.sleep(5)
 
-#Go to checkout page
-driver.find_element(By.CSS_SELECTOR, ".btn.btn-success").click()
-driver.find_element(By.ID, "country").send_keys("Un")
 
-wait = WebDriverWait(driver, 10)
-countries = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[class='suggestions'] ul li")))
-
-for country in countries:
-    if country.text.strip() == 'United States of America':
-        country.click()
-        break
-
-driver.find_element(By.CSS_SELECTOR, "label[for='checkbox2']").click()
-driver.find_element(By.CSS_SELECTOR, "input[value='Purchase']").click()
-notif = driver.find_element(By.CSS_SELECTOR, ".alert.alert-success.alert-dismissible").text.strip()
-assert "Success! Thank you! Your order will be delivered in next few weeks :-)." in notif
-
-time.sleep(5)
+@pytest.mark.parametrize("test_data", test_data)
+def test_get_title(browser_instance, test_data):
+    driver = browser_instance
+    login_page = LoginPage(driver)
+    assert login_page.get_title() == "LoginPage Practise | Rahul Shetty Academy"
+    login_page.login(test_data["username"], test_data["password"])
+    time.sleep(3)
+    shop_page = ProductSelection(driver, test_data["product_name"])
+    assert shop_page.get_title() == "ProtoCommerce"
